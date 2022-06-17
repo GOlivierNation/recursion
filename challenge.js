9b214de22066a4a4e8836cb0f34d385a9e51261f
GOlivierNation <ogasominali@gmail.com>
2022-06-17 09:45:00 +0200

    Recursion Challenge

---
+++ challenge.js
@@ -0,0 +1,10 @@
+module.exports = sum = (number) => {
+    // write your code here
+    if (number <= 0) {
+      return 0;
+    }
+    return number + sum(number - 1);
+  };
+  
+  console.log(sum(4));
+  console.log(sum(10));
